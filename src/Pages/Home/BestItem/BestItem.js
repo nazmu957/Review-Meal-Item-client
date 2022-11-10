3b395ee0bc1798b0e880559f4b06edb949ef0571
nazmu957 <nazmus15-1999@diu.edu.bd>
2022-11-10 21:43:24 +0600

    most wanted headline updated

--- src/Pages/Home/BestItem/BestItem.js
+++ src/Pages/Home/BestItem/BestItem.js
@@ -6,7 +6,7 @@ const BestItem = () => {
        <div className="card items-center   bg-base-100 shadow-xl">
   <figure><img className='w-2/3 shadow-2xl rounded-b-lg rounded-t-lg' src={bestFood}/></figure>
   <div className="card-body">
-    <h2 className="card-title">Most Wanted item!</h2>
+    <h2 className="card-title text-2xl text-red-700">Most Wanted item!</h2>
   </div>
 </div>
     );
