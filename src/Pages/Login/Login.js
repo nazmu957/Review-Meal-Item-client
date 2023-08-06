2f9a94650e4affed10e9bef0a16360b26b7fb688
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-08-06 18:09:35 +0600

    login page text align issu fxied

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -58,7 +58,7 @@ const Login = () => {
           </label>
           <input type="password" name="password" placeholder="password" className="input input-bordered" />
           <label className="label">
-             <p>New in our site?<Link to='/signup' className="label-text-alt link link-hover text-sm text-blue-500 font-bold">Create Account</Link></p>
+             <p className="text-sm py-2">New in our site?<Link to='/signup' className="label-text-alt link link-hover text-sm text-blue-500 font-bold">Create Account</Link></p>
           </label>
           <button onClick={handleGoogleSignIn} className="btn btn-outline btn-success">Google Sign In</button>
         </div>
