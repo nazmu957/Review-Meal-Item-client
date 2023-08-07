d8c9496ecf1be0cba0616dd997a223258245dbd7
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-08-07 22:02:59 +0600

    best product updated

--- src/Pages/Home/BestItem/BestItem.js
+++ src/Pages/Home/BestItem/BestItem.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from 'react'
 import bestFood from '../../../images/vipfood.jpg'
 
 const BestItem = () => {
-    return (
-       <div className="card items-center   bg-base-100 shadow-xl">
-  <figure><img className='w-2/3 shadow-2xl rounded-b-lg rounded-t-lg' src={bestFood}/></figure>
-  <div className="card-body">
-    <h2 className="card-title text-2xl text-red-700">Most Wanted item!</h2>
-  </div>
-</div>
-    );
-};
+  return (
+    <div className="card items-center   bg-base-100 shadow-xl mt-5">
+      <figure>
+        <img
+          className="w-2/3 shadow-2xl rounded-b-lg rounded-t-lg"
+          src={bestFood}
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title text-2xl text-red-700">Most Wanted item!</h2>
+      </div>
+    </div>
+  )
+}
 
-export default BestItem;
+export default BestItem
--- src/Pages/Home/Services/SameReview.js
+++ src/Pages/Home/Services/SameReview.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React from 'react'
+import { useLoaderData } from 'react-router-dom'
 
-const SameReview = ({review}) => {
-    const service = useLoaderData();
-    const { title } = service;
-    const {_id,serviceName,message,photoUrl,customer} = review;
-    console.log(review);
+const SameReview = ({ review }) => {
+  const service = useLoaderData()
+  const { title } = service
+  const { _id, serviceName, message, photoUrl, customer } = review
+  console.log(review)
 
+  return (
+    <div>
+      <div className="card card-side bg-base-100 shadow-xl mb-5 mr-10">
+        <figure>
+          <img
+            className="w-24 h-24 rounded-full ml-5"
+            src={photoUrl}
+            alt="Movie"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{customer}</h2>
+          <p>{message}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
 
-    return (
-       <div>
-          
-           <div className="card card-side bg-base-100 shadow-xl mb-5 mr-10">
-    <figure><img className='w-24 h-24 rounded-full ml-5' src={photoUrl} alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">{customer}</h2>
-    <p>{message}</p>
-    
-  </div>
-</div>
-       </div>
-    );
-};
-
-export default SameReview;
+export default SameReview
