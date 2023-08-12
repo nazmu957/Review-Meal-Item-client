c9c9d90b681aef4d4a3454519d0e6aa3945f5260
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-08-12 08:27:51 +0600

    solve auto carousel bug

--- src/Pages/Home/Home/Banner/Banner.js
+++ src/Pages/Home/Home/Banner/Banner.js
@@ -1,50 +1,58 @@
-import React from 'react';
 import banner1 from '../../../../images/banner1.jpg'
 import banner2 from '../../../../images/banner2.webp'
 import banner3 from '../../../../images/banner3.webp'
 import banner5 from '../../../../images/banner5.webp'
 import banner6 from '../../../../images/banner6.webp'
 import banner7 from '../../../../images/banner7.webp'
-
-
 import banner4 from '../../../../images/banner4.jpg'
-
+import React, { useState, useEffect } from 'react';
 
 const Banner = () => {
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  const slides = [
+    { id: 1, image: banner5 },
+    { id: 2, image: banner6 },
+    { id: 3, image: banner3 },
+    { id: 4, image: banner7 },
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlide) => (prevSlide % slides.length) + 1);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
     return (
-        <div>
-            <div className="carousel w-full">
-  <div id="slide1" className="carousel-item relative w-full">
-    <img src={banner5} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide4" className="btn btn-circle">❮</a> 
-      <a href="#slide2" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide2" className="carousel-item relative w-full">
-    <img src={banner6} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide1" className="btn btn-circle">❮</a> 
-      <a href="#slide3" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide3" className="carousel-item relative w-full">
-    <img src={banner3} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide2" className="btn btn-circle">❮</a> 
-      <a href="#slide4" className="btn btn-circle">❯</a>
-    </div>
-  </div> 
-  <div id="slide4" className="carousel-item relative w-full">
-    <img src={banner7} className="w-full" />
-    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-      <a href="#slide3" className="btn btn-circle">❮</a> 
-      <a href="#slide1" className="btn btn-circle">❯</a>
-    </div>
-  </div>
-</div>
+    
+      <div className="carousel w-full">
+      {slides.map((slide) => (
+        <div
+          key={slide.id}
+          className={`carousel-item relative w-full ${
+            slide.id === currentSlide ? 'block' : 'hidden'
+          }`}
+        >
+          <img src={slide.image} className="w-full" />
+          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+            <a
+              href={`#slide${slide.id === 1 ? slides.length : slide.id - 1}`}
+              className="btn btn-circle"
+            >
+              ❮
+            </a>
+            <a
+              href={`#slide${slide.id === slides.length ? 1 : slide.id + 1}`}
+              className="btn btn-circle"
+            >
+              ❯
+            </a>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Banner;
