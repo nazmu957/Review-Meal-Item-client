c961a112413434bdf18eea2dabc6bb4f04d9e257
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-08-13 12:12:33 +0600

    all Service component add a container

--- src/Pages/Home/Services/AllServices.js
+++ src/Pages/Home/Services/AllServices.js
@@ -9,7 +9,7 @@ const AllServices = () => {
       .then((data) => setAllServices(data))
   }, [])
   return (
-    <div>
+    <div class="container mx-auto px-4">
       <h2 className='text-4xl text-center p-5'>All Food Items</h2>
       <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {allServices.map((service) => (
--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
   return (
     <div class="container mx-auto px-4">
       <div className="text-center mb-4 mt-8">
-        <p className="text-3xl font-bold py-5">Our Delicious Food Item</p>
+        <p className="text-2xl font-bold py-3">Our Delicious Food Item</p>
       </div>
       <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {services.map((service) => (
