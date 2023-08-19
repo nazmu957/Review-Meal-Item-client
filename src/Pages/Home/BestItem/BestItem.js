d3e4448d5c7811c74aa693d68db9e6042ae56aa3
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-08-19 12:29:20 +0600

    remove best item from home page

--- src/Pages/Home/BestItem/BestItem.js
+++ src/Pages/Home/BestItem/BestItem.js
@@ -3,17 +3,7 @@ import bestFood from '../../../images/vipfood.jpg'
 
 const BestItem = () => {
   return (
-    <div className="card items-center   bg-base-100 shadow-xl mt-5">
-      <figure>
-        <img
-          className="w-2/3 shadow-2xl rounded-b-lg rounded-t-lg"
-          src={bestFood}
-        />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title text-2xl text-red-700">Most Wanted item!</h2>
-      </div>
-    </div>
+    <div></div>
   )
 }
 
