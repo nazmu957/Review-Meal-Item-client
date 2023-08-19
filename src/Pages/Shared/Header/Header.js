73da847d7bb16c5e29bbdd6b1994973481d34d80
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-08-19 16:27:09 +0600

    change the user profile position

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -5,102 +5,77 @@ import logo from '../../../images/logo.jpeg'
 import { FaUserAlt } from "react-icons/fa";
 
 const Header = () => {
-    const {user,logOut} = useContext(AuthContext)
-    // const menuItems = <>
-    //     <li className='font-semibold '><Link to='/'>Home</Link></li>
-    //   <li className='font-semibold '><Link to='/blog'>Blog</Link></li>
-     
-      
-    // </>
-
+    const { user, logOut } = useContext(AuthContext)
 
     return (
-        <div className="navbar  bg-zinc-200">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-      </label>
-      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-        <li className='font-semibold '><Link to='/'>Home</Link></li>
-        <li className='font-semibold '><Link to='/blog'>About Us</Link></li>
+        <div className="navbar bg-zinc-200">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li className='font-semibold '><Link to='/'>Home</Link></li>
+                        <li className='font-semibold '><Link to='/blog'>About Us</Link></li>
 
-        {
-      user?.uid ?
-      
-          
-            <>
-            {/* <li  className="text-sm font-semibold "><Link to="/addService">Add Service</Link></li> */}
-            <li  className="text-sm font-semibold "><Link to="/myReview">My Review</Link></li>
-            <button onClick={logOut} className="text-sm font-semibold  mr-28">LogOut</button>
-            </>
-          
-      :
-     <>
-      <li  className="text-sm font-semibold "><Link to="/signup">SignUp</Link></li>
-      <li  className="text-sm font-semibold "><Link to="/login">Login</Link></li>
-     
-      
-     </>
-    }
-    {
-      user?.photoURL ?
-     <>
-     {/* <h2>{user?.displayName}</h2> */}
-     <img className='rounded-full' style={{height: '40px', width:'40px'}} src={user.photoURL} alt=""/>
-     </>
-      :
-      <FaUserAlt className="text-dark ml-3 " style={{ height: '20px', width:'20px' }}></FaUserAlt>
-    }
-      </ul>
-    </div>
-    <Link to='/' className="btn btn-ghost normal-case text-xl">
-        <img src={logo} width="48px"  alt=""/>
-    </Link>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal p-0">
-        <li className='font-semibold '><Link to='/blog'>About Us</Link></li>
-          <li className='font-semibold '><Link to='/'>Home</Link></li>
+                        {
+                            user?.uid ?
 
-          {
-      user?.uid ?
-      
-          
-            <>
-           {/* <li  className="text-sm font-semibold "><Link to="/addService">Add Service</Link></li> */}
-            <li  className="text-sm font-semibold "><Link to="/myReview">My Review</Link></li>
-            <button onClick={logOut} className="text-sm font-semibold ">LogOut</button>
-            </>
-          
-      :
-     <>
-      <li  className="text-sm font-semibold "><Link to="/signup">SignUp</Link></li>
-      <li  className="text-sm font-semibold "><Link to="/login">Login</Link></li>
-      
-     </>
-    }
-    {
-      user?.photoURL ?
-     <>
-     {/* <h2>{user?.displayName}</h2> */}
-     <img className='rounded-full ml-5' style={{height: '40px', width:'40px'}} src={user.photoURL} alt=""/>
-     </>
-      :
-     <FaUserAlt className="text-dark mt-3" style={{ height: '20px', width:'20px' }}></FaUserAlt>
-    }
-    </ul>
-  </div>
-  
-  <div className="navbar-end">
+                                <>
+                                    {/* <li  className="text-sm font-semibold "><Link to="/addService">Add Service</Link></li> */}
+                                    <li className="text-sm font-semibold "><Link to="/myReview">My Review</Link></li>
+                                    <button onClick={logOut} className="text-sm font-semibold  mr-28">LogOut</button>
+                                </>
 
-    
+                                :
+                                <>
+                                    <li className="text-sm font-semibold "><Link to="/signup">SignUp</Link></li>
+                                    <li className="text-sm font-semibold "><Link to="/login">Login</Link></li>
 
-    
-   
-    
-  </div>
-</div>
+
+                                </>
+                        }
+                    </ul>
+                </div>
+                <Link to='/' className="btn btn-ghost normal-case text-xl">
+                    <img src={logo} width="48px" alt="" />
+                </Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal p-0">
+                    <li className='font-semibold '><Link to='/blog'>About Us</Link></li>
+                    <li className='font-semibold '><Link to='/'>Home</Link></li>
+
+                    {
+                        user?.uid ?
+
+                            <>
+                                {/* <li  className="text-sm font-semibold "><Link to="/addService">Add Service</Link></li> */}
+                                <li className="text-sm font-semibold "><Link to="/myReview">My Review</Link></li>
+                                <button onClick={logOut} className="text-sm font-semibold ">LogOut</button>
+                            </>
+
+                            :
+                            <>
+                                <li className="text-sm font-semibold "><Link to="/signup">SignUp</Link></li>
+                                <li className="text-sm font-semibold "><Link to="/login">Login</Link></li>
+
+                            </>
+                    }
+                </ul>
+            </div>
+
+            <div className="navbar-end">
+                <div className="flex flex-row-reverse items-center">
+                    {
+                        user?.photoURL ?
+                            <img className='rounded-full ml-5' style={{ height: '40px', width: '40px' }} src={user.photoURL} alt="" />
+                            :
+                            <FaUserAlt className="text-dark mt-3" style={{ height: '20px', width: '20px' }}></FaUserAlt>
+                    }
+                </div>
+            </div>
+        </div>
     );
 };
 
