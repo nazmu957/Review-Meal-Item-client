7dc970b330d6a923b3a31ecfa5a1fc02a4c0f496
nazmu957 <nazmus15-1999@diu.edu.bd>
2023-09-04 17:29:08 +0600

    change card elemnt

--- src/Pages/Home/Services/ServiceCard.js
+++ src/Pages/Home/Services/ServiceCard.js
@@ -3,6 +3,7 @@ import { PhotoProvider, PhotoView } from "react-photo-view";
 import "react-photo-view/dist/react-photo-view.css";
 import { Link } from "react-router-dom";
 import { AiFillStar } from "react-icons/ai";
+import { FaPlus } from "react-icons/fa";
 
 
 const ServiceCard = ({ service }) => {
@@ -38,22 +39,26 @@ const ServiceCard = ({ service }) => {
             </PhotoProvider>
           </figure>
           <div className="card-body pt-0">
-            <h2 className="card-title text-base">{title} </h2>
-            <p className="text-lg font-semibold text-base">${price}</p>
+            <h2 className="card-title text-2xl">{title} </h2>
+            <button className=" font-bold text-white bg-red-600 text-start w-[5.5rem] px-[.5rem] py-1 rounded-lg">
+              ${price}
+            </button>
             <p className="text-sm">Delivery Charge: $5</p>
             <div className="flex text-orange-500 text-sm">
               <AiFillStar />
               <AiFillStar />
               <AiFillStar />
               <AiFillStar />
-              
             </div>
             <div className="card-actions ">
-              {/* <Link to={`/services/${_id}`}>
-                <button className="btn btn-sm btn-outline btn-info">
-                  View Details
-                </button>
-              </Link> */}
+              <Link to={`/services/${_id}`}>
+                <div className="flex  hover:text-red-600">
+                  <FaPlus className="text-sm mt-[.4rem]" />
+                  <button className=" ml-2 font-semibold">
+                    Add your review
+                  </button>
+                </div>
+              </Link>
             </div>
           </div>
         </div>
